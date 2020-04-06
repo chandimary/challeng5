69e769cf9e284b1dd1daa7885fb0e61def0c89e0
chandimary <chandimarygloria@gmail.com>
2020-04-06 18:43:08 +0300

    core

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 function revealMessage(){
     document.getElementById("hiddenMessage").style.display='block';
+} 
+
+
+function checkForm(){
+    var email =document.getElementById("email");
+    var password = document.getElementById(password);
+
+    if (email.value=='' || !isNaN(emeail)|| !email.matches(/^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]+$/));{
+        emeailCanSubmit = false;
+        alert('Enter your enail');
+        
+    };
+   return true; 
+    
 }
+
+
+
